c6668546232e969c63899e5805faa9aeb34e273c
Tarumi <taryunelab@gmail.com>
2021-02-15 15:55:47 +0900

    add format

--- tasks.py
+++ tasks.py
@@ -5,3 +5,7 @@ import invoke
 def setup(c):
     invoke.run("pipenv install --dev")
     invoke.run("pipenv run pre-commit install")
+
+@invoke.task
+def format(c):
+    invoke.run("pre-commit run --all-files")
